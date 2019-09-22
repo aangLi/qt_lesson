91fc20973fc27b13731349f2bf6ab3a94e6ef392
aangLi <517249177@qq.com>
2019-09-22 14:36:09 +0800

    组件的模块化

---
+++ vue/vue-facework/src/components/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="menu">
+    <slot></slot>
+  </ul>
+</template>
+<style >
+ul
+{
+  margin: 0;
+  padding: 0
+}
+
+li{
+  list-style: none
+}
+.menu{
+  width: 200px
+}
+.menu li{
+    padding: 5px;
+    background-color: #4cb4e7;
+    color: #ffffff;
+    border-bottom: 1px solid #ffffff
+}
+
+
+</style>
