f39ae4569583402db8eb49b265c53907de694e7a
aangLi <517249177@qq.com>
2019-09-27 21:41:58 +0800

    手写element-ui-col & row

--- vue/vue-layout-h/src/App.vue
+++ vue/vue-layout-h/src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+<div id="App">
+  <el-button :plain="true" @click="openVn">VNode</el-button>
+  <my-elrow tag="div" :jiange="20">
+    <!-- 24 -->
+    <my-elcow :span="4">cle-out</my-elcow>
+    <my-elcow :span="16" :offset="4">clecow-out</my-elcow>
+  </my-elrow>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import ElRow from './components/El-Row'
+import ElCol from './components/El-Col'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    'my-elrow':ElRow,
+
+    'my-elcow':ElCol
+  },
+  methods: {
+    openVn() {
+        const h = this.$createElement;
+        this.$message({
+          message: h('p', null, [
+            h('span', null, '内容可以是 '),
+            h('i', { style: 'color: teal' }, 'VNode')
+          ]),
+          duration: 0
+        });
+      }
+  },
 }
 </script>
 
---
+++ vue/vue-layout-h/src/components/El-Row.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: {
+    tag: {
+      type: String, default: 'div'
+    },
+    jiange: {
+      type: Number
+    }
+  },
+  render (h) {
+    // h === createElement
+    return h(
+      this.tag,
+      {
+        class: ['el-row']
+      },
+      this.$slots.default
+    )
+  }
+}
+</script>
+<style lang="">
+  .el-row {
+    width: 100%;
+    display: flex;
+  }
+</style>
