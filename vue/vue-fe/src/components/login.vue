0da48f658bce4c5dc2ad1a72fa17915baf87c2f1
aangLi <517249177@qq.com>
2019-10-19 16:31:23 +0800

    vue-fe 前端和后端结合

---
+++ vue/vue-fe/src/components/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="text" v-model="name" />
+    <input type="text" v-model="password" />
+    <button @click="login">登录</button>
+  </div>
+</template>
+
+<script>
+import Axios from 'axios'
+export default {
+  data() {
+    return {
+      name: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      Axios.post('/api/v1/login', {
+        name: this.name,
+        password: this.password
+      }).then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
