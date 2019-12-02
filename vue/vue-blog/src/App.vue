743d18e6916c93aea6f018875c0e9c16efb7d104
aangLi <517249177@qq.com>
2019-12-02 22:00:23 +0800

    'vue-blog'

---
+++ vue/vue-blog/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <Nav></Nav> 
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import { Vue, Component } from "vue-property-decorator";
+  import Nav from '@/components/nav'
+
+  @Component({
+    components: {
+      Nav
+    }
+  })
+
+  class App extends Vue {
+
+  }
+  export default App;
+</script> 
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
