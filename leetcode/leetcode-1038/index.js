87c26d67d6664b7e110ee82588e4f47df754286e
aangLi <517249177@qq.com>
2020-02-08 15:32:01 +0800

    'leetcode-1038'

--- leetcode/leetcode-1038/index.js
+++ leetcode/leetcode-1038/index.js
@@ -5,20 +5,37 @@
  *     this.left = this.right = null;
  * }
  */
-let i = 0;
-let len = 0;
-function TreeNode(root) {
-  console.log(root)
-  if (i * 2 + 1 > len) {
-    return this;
+/**
+ * 
+ * @param {Number[]} val 
+ */
+function TreeNode(val) {
+  if (val === null) {
+    this.val = null;
+  } else {
+    this.val = val;
+    this.left = this.right = null;
   }
-  // console.log()
-  console.log(`i: ${i} -----> this: ${this} ------> root[i]: ${root[i]}`)
-  this.val = root[i];
-  this.left = TreeNode(root[i*2]);
-  this.right = TreeNode(root[i*2 + 1])
-  i ++;
 }
+
+function Tree(root) {
+  root.filter((element, index) => {
+    root[index] = new TreeNode(element);
+    // console.log('root: ')
+    // console.log(root)
+  })
+  
+  root.filter((Element, index) => {
+    if (Element.val !== null) {
+      Element.right = (index + 1) * 2 > root.length - 1 ? "";
+      Element.left = root[(index + 1) * 2 - 1];
+    }
+  })
+  return root[0];
+  
+}
+
+Tree([4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]);
 
 /**
  * @param {TreeNode} root
@@ -26,23 +43,26 @@ function TreeNode(root) {
  */
 let sum = 0;
 var bstToGst = function(root) {
-    len = root.length;
-    i = 0;
     console.log(root)
-    let rt = TreeNode(root)
+    let rt = Tree(root)
 
     console.log(rt)
 
-    // sum = 0;
-    // if (root == null) {
-    //   // return null;
-    // }
-    // bstToGstCore(root)
-    // return root;
+    sum = 0;
+    if (rt == null) {
+      return null;
+    }
+    bstToGstCore(rt)
+    return rt;
 
 };
 
-function bstToGstCore (node) {
+var bstToGstCore = function(node) {
+  console.log('node');
+  console.log(node)
+  if (node.val === null) {
+    return;
+  }
   if (node.right !== null) {
     bstToGstCore(node.right);
   }
