47fb79d232bd637da3a841e2323efe8530cf0b25
aangLi <517249177@qq.com>
2020-02-16 20:25:04 +0800

    'leetcode-226'

---
+++ leetcode/树相关题目/leetcode-226/index.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(root.val === null) 
+      return null;
+    root.left = invertTree(root.left);
+    root.right = invertTree(root.right);
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    return root;
+};
